9295ce05aa368067cfdca28f646f6787eb8efb28
myriame <myriameayman@gmail.com>
2015-11-28 13:40:08 -0500

    adding classes

---
+++ src/ROB.java
@@ -0,0 +1,31 @@
+
+public class ROB {
+	RowROB [] array;
+	int head; 
+	int tail;
+	public ROB(int size) {
+		array = new RowROB[size];
+		head = -1;
+		tail = 0; 
+	}
+	public boolean push(RowROB row) {
+		if ((head == 0 && tail == array.length - 1) || head - tail == 1) 
+			return false;
+		array[tail] = new RowROB(row.insType,row.dest, row.value,row.ready);
+		tail ++; 
+		if(tail == array.length) 
+			tail = array.length -1; 
+		if (head == -1)
+			head = 0;
+		return true;
+	}
+	public RowROB pop() {
+		if (head == -1 || head == tail)
+			return null; 
+		RowROB temp = array[head];
+		head ++; 
+		if (head == array.length)
+			head = 0;
+		return temp;
+	}
+}
--- src/Run.java
+++ src/Run.java
@@ -1,10 +1,17 @@
 import java.util.ArrayList;
 
 public class Run {
-	ArrayList<Integer> registersFile = new ArrayList<Integer>();
+	int PC;
+	ArrayList<String> registersFile = new ArrayList<String>();
 	ArrayList<ArrayList<Stage>> julie = new ArrayList<ArrayList<Stage>>();
 	ArrayList<Integer> registerStatus = new ArrayList<Integer>();
-	ArrayList<ArrayList<RowScoreboard>> scoreBoard; 
-	ArrayList<ArrayList<RowROB>> rob; 
-	
+	ArrayList<ArrayList<RowScoreboard>>  scoreboard = new ArrayList<ArrayList<RowScoreboard>>();  
+	ROB rob; 	
+	public Run(ArrayList<String> ins,int numberOfEntryROB) {
+		for(int i = 0; i < 8; i++) 
+			registersFile.add("00");
+		for (int i =0; i < 8; i++) 
+			registerStatus.add(-1);
+		rob = new ROB(numberOfEntryROB);
+	}
 }
