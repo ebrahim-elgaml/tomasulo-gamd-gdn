2b0a9c7fa0261e40fe083c426cc0ceaef57f4186
maggie-moheb <maggiemoheb@gmail.com>
2015-12-05 19:49:59 +0200

    adding helpers

--- src/Helper.java
+++ src/Helper.java
@@ -28,6 +28,44 @@ public class Helper {
 		
 		return null;
 	}
+	public static int hexToDecimal(String hex) {
+		int result = 0;
+		for(int i = 0; i< hex.length(); i++) {
+			String character = hex.charAt(i)+"";
+			switch(character)  {
+				case("F"): result += 15*Math.pow(16, i);break;
+				case("E"): result += 14*Math.pow(16, i);break;
+				case("D"): result += 13*Math.pow(16, i);break;
+				case("C"): result += 12*Math.pow(16, i);break;
+				case("B"): result += 11*Math.pow(16, i);break;
+				case("A"): result += 10*Math.pow(16, i);break;
+				default: result+= Integer.parseInt(character)*Math.pow(16, i);
+			}	
+		}
+		return result;
+	}
+	public static String decimalToHex(int decimal) {
+		String s = "";
+		do{
+			int x= decimal%16;
+			switch(x) {
+				case(10): s+='A';break;
+				case(11): s+='B';break;
+				case(12): s+='C';break;
+				case(13): s+='D';break;
+				case(14): s+='E';break;
+				case(15): s+='F';break;
+				default: s+=x;
+			}
+			decimal /=16;
+		}while(decimal>0);
+		String sRev = "";
+		for(int i = 0; i<s.length();i++) {
+			sRev += s.charAt(i);
+		}
+		return sRev;
+	}
+	
 	public static Instruction stringToInstruction(String s){
 		String[] inst = s.split(" ");
 		Type t = toType(inst[0]);
@@ -42,4 +80,5 @@ public class Helper {
 		}
 		return new Instruction(t, regA, regB, imm);
 	}
+	
 }
--- src/Instruction.java
+++ src/Instruction.java
@@ -3,11 +3,15 @@ public class Instruction {
 	int regA;
 	int regB;
 	int imm;
-
+	
 	public Instruction(Type t, int rA, int rB, int imm) {
 		this.type = t;
 		this.regA = rA;
 		this.regB = rB;
 		this.imm = imm;
+	}
+	
+	public void load() {
+		
 	}
 }
