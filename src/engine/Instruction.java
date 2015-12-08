dabe3d1c38bd40207039f9e76572d8ba6ad3a7d4
Mark Morcos <mark.yehia@gmail.com>
2015-12-08 18:55:36 +0200

    Taken input from user and fixed some engine bugs

--- src/engine/Instruction.java
+++ src/engine/Instruction.java
@@ -1,4 +1,5 @@
 package engine;
+
 public class Instruction {
 	Type type;
 	int regA;
@@ -7,194 +8,205 @@ public class Instruction {
 	int indexOfInstruction;
 	int addressOfInstruction;
 	int noOfCycles;
-	public Instruction(Type t,int addressOfInstruction, int rA, int rB, int imm){
+
+	public Instruction(Type t, int addressOfInstruction, int rA, int rB, int imm) {
 		this.addressOfInstruction = addressOfInstruction;
-		this.indexOfInstruction = addressOfInstruction-Run.origin;
+		this.indexOfInstruction = addressOfInstruction - Run.origin;
 		this.type = t;
 		this.regA = rA;
 		this.regB = rB;
 		this.imm = imm;
 		this.noOfCycles = Run.instructionCycles.get(t);
 	}
-	public void execute(){
-		if(this.type == Type.LW){
+
+	public void execute() {
+		if (this.type == Type.LW) {
 			this.load();
-			return ;
+			return;
 		}
-		if(this.type == Type.SW){
+		if (this.type == Type.SW) {
 			this.store();
-			return ;
+			return;
 		}
-		if(this.type == Type.BEQ){
+		if (this.type == Type.BEQ) {
 			this.branch();
-			return ;
+			return;
 		}
-		if(this.type == Type.ADD){
+		if (this.type == Type.ADD) {
 			this.add();
-			return ;
+			return;
 		}
-		if(this.type == Type.SUB){
+		if (this.type == Type.SUB) {
 			this.sub();
-			return ;
+			return;
 		}
-		if(this.type == Type.ADDI){
+		if (this.type == Type.ADDI) {
 			this.addi();
-			return ;
+			return;
 		}
-		if(this.type == Type.NAND){
+		if (this.type == Type.NAND) {
 			this.nand();
-			return ;
+			return;
 		}
-		if(this.type == Type.MUL){
+		if (this.type == Type.MUL) {
 			this.mul();
-			return ;
+			return;
 		}
 	}
+
 	public void load() {
 		String baseAddress = Run.registersFile.get(regB);
 		int address = imm + Helper.hexToDecimal(baseAddress);
-		int julieColumn = this.addressOfInstruction-Run.origin;
+		int julieColumn = this.addressOfInstruction - Run.origin;
 		Run.julie.get(Run.clock).add(julieColumn, Stage.EXEC);
-		if(noOfCycles == -1) {
+		if (noOfCycles == -1) {
 			noOfCycles = MemoryHandler.dataCache.readCycles(address);
 		}
-		if(noOfCycles>0) {
+		if (noOfCycles > 0) {
 			noOfCycles--;
 			return;
 		}
-		for(int i = 0; i<Run.scoreboard.size(); i++) {
-			if(Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
-				Run.scoreboard.get(i).result = MemoryHandler.read(address);
-				Run.scoreboard.get(i).address = address;
-				break;
-			}
-		}
-		return; 	
-	}
-	public void store() {
-		String baseAddress = Run.registersFile.get(regB);
-		int address = imm + Helper.hexToDecimal(baseAddress);
-		int julieColumn = this.addressOfInstruction-Run.origin;
-		Run.julie.get(Run.clock).add(julieColumn, Stage.EXEC);
-		for(int i = 0; i<Run.scoreboard.size(); i++) {
-			if(Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
-				Run.scoreboard.get(i).result =  Run.registersFile.get(regA);
+		for (int i = 0; i < Run.scoreboard.size(); i++) {
+			if (Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
+				Run.scoreboard.get(i).result = MemoryHandler.readData(address);
 				Run.scoreboard.get(i).address = address;
 				break;
 			}
 		}
 		return;
 	}
-	public void branch() {
-		int julieColumn = this.addressOfInstruction-Run.origin;
+
+	public void store() {
+		String baseAddress = Run.registersFile.get(regB);
+		int address = imm + Helper.hexToDecimal(baseAddress);
+		int julieColumn = this.addressOfInstruction - Run.origin;
 		Run.julie.get(Run.clock).add(julieColumn, Stage.EXEC);
-		if(noOfCycles>0) {
+		for (int i = 0; i < Run.scoreboard.size(); i++) {
+			if (Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
+				Run.scoreboard.get(i).result = Run.registersFile.get(regA);
+				Run.scoreboard.get(i).address = address;
+				break;
+			}
+		}
+		return;
+	}
+
+	public void branch() {
+		int julieColumn = this.addressOfInstruction - Run.origin;
+		Run.julie.get(Run.clock).add(julieColumn, Stage.EXEC);
+		if (noOfCycles > 0) {
 			noOfCycles--;
 			return;
 		}
 		String value1 = Run.registersFile.get(regA);
 		String value2 = Run.registersFile.get(regB);
-		if(value1.equals(value2)) {
-			for(int i = 0; i<Run.scoreboard.size(); i++) {
-				if(Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
-					Run.scoreboard.get(i).result =  "true";
-					Run.scoreboard.get(i).address = addressOfInstruction+1+imm;
+		if (value1.equals(value2)) {
+			for (int i = 0; i < Run.scoreboard.size(); i++) {
+				if (Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
+					Run.scoreboard.get(i).result = "true";
+					Run.scoreboard.get(i).address = addressOfInstruction + 1
+							+ imm;
 					break;
 				}
 			}
-		}else {
-			for(int i = 0; i<Run.scoreboard.size(); i++) {
-				if(Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
-					Run.scoreboard.get(i).address = addressOfInstruction+1;
-					Run.scoreboard.get(i).result =  "false";
+		} else {
+			for (int i = 0; i < Run.scoreboard.size(); i++) {
+				if (Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
+					Run.scoreboard.get(i).address = addressOfInstruction + 1;
+					Run.scoreboard.get(i).result = "false";
 					break;
 				}
-			}	
+			}
 		}
 	}
-	
-	public void add(){
-		int julieColumn = this.addressOfInstruction-Run.origin;
+
+	public void add() {
+		int julieColumn = this.addressOfInstruction - Run.origin;
 		Run.julie.get(Run.clock).add(julieColumn, Stage.EXEC);
-		if(noOfCycles>0) {
+		if (noOfCycles > 0) {
 			noOfCycles--;
 			return;
 		}
 		int value1 = Helper.hexToDecimal(Run.registersFile.get(regB));
 		int value2 = Helper.hexToDecimal(Run.registersFile.get(imm));
-		for(int i = 0; i<Run.scoreboard.size(); i++) {
-			if(Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
-				Run.scoreboard.get(i).result =  Helper.decimalToHex((value1+value2)&255);
+		for (int i = 0; i < Run.scoreboard.size(); i++) {
+			if (Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
+				Run.scoreboard.get(i).result = Helper
+						.decimalToHex((value1 + value2) & 255);
 				break;
 			}
 		}
 	}
-	
-	public void sub(){
-		int julieColumn = this.addressOfInstruction-Run.origin;
+
+	public void sub() {
+		int julieColumn = this.addressOfInstruction - Run.origin;
 		Run.julie.get(Run.clock).add(julieColumn, Stage.EXEC);
-		if(noOfCycles>0) {
+		if (noOfCycles > 0) {
 			noOfCycles--;
 			return;
 		}
 		int value1 = Helper.hexToDecimal(Run.registersFile.get(regB));
 		int value2 = Helper.hexToDecimal(Run.registersFile.get(imm));
-		for(int i = 0; i<Run.scoreboard.size(); i++) {
-			if(Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
-				Run.scoreboard.get(i).result =  Helper.decimalToHex((value1-value2)&255);
+		for (int i = 0; i < Run.scoreboard.size(); i++) {
+			if (Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
+				Run.scoreboard.get(i).result = Helper
+						.decimalToHex((value1 - value2) & 255);
 				break;
 			}
 		}
 	}
-	
-	public void mul(){
-		int julieColumn = this.addressOfInstruction-Run.origin;
+
+	public void mul() {
+		int julieColumn = this.addressOfInstruction - Run.origin;
 		Run.julie.get(Run.clock).add(julieColumn, Stage.EXEC);
-		if(noOfCycles>0) {
+		if (noOfCycles > 0) {
 			noOfCycles--;
 			return;
 		}
 		int value1 = Helper.hexToDecimal(Run.registersFile.get(regB));
 		int value2 = Helper.hexToDecimal(Run.registersFile.get(imm));
-		for(int i = 0; i<Run.scoreboard.size(); i++) {
-			if(Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
-				Run.scoreboard.get(i).result =  Helper.decimalToHex((value1*value2)&255);
+		for (int i = 0; i < Run.scoreboard.size(); i++) {
+			if (Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
+				Run.scoreboard.get(i).result = Helper
+						.decimalToHex((value1 * value2) & 255);
 				break;
 			}
 		}
 	}
-	
-	public void nand(){
-		int julieColumn = this.addressOfInstruction-Run.origin;
+
+	public void nand() {
+		int julieColumn = this.addressOfInstruction - Run.origin;
 		Run.julie.get(Run.clock).add(julieColumn, Stage.EXEC);
-		if(noOfCycles>0) {
+		if (noOfCycles > 0) {
 			noOfCycles--;
 			return;
 		}
 		int value1 = Helper.hexToDecimal(Run.registersFile.get(regB));
 		int value2 = Helper.hexToDecimal(Run.registersFile.get(imm));
-		for(int i = 0; i<Run.scoreboard.size(); i++) {
-			if(Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
-				Run.scoreboard.get(i).result =  Helper.decimalToHex((value1&value2)^255);
+		for (int i = 0; i < Run.scoreboard.size(); i++) {
+			if (Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
+				Run.scoreboard.get(i).result = Helper
+						.decimalToHex((value1 & value2) ^ 255);
 				break;
 			}
 		}
 	}
-	public void addi(){
-		int julieColumn = this.addressOfInstruction-Run.origin;
+
+	public void addi() {
+		int julieColumn = this.addressOfInstruction - Run.origin;
 		Run.julie.get(Run.clock).add(julieColumn, Stage.EXEC);
-		if(noOfCycles>0) {
+		if (noOfCycles > 0) {
 			noOfCycles--;
 			return;
 		}
 		int value1 = Helper.hexToDecimal(Run.registersFile.get(regB));
-		for(int i = 0; i<Run.scoreboard.size(); i++) {
-			if(Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
-				Run.scoreboard.get(i).result =  Helper.decimalToHex((value1+this.imm)&255);
+		for (int i = 0; i < Run.scoreboard.size(); i++) {
+			if (Run.scoreboard.get(i).instructionAddress == this.addressOfInstruction) {
+				Run.scoreboard.get(i).result = Helper
+						.decimalToHex((value1 + this.imm) & 255);
 				break;
 			}
 		}
 	}
-	
 
 }
--- src/engine/MemoryHandler.java
+++ src/engine/MemoryHandler.java
@@ -1,4 +1,5 @@
 package engine;
+
 import java.util.ArrayList;
 
 public class MemoryHandler {
@@ -6,33 +7,54 @@ public class MemoryHandler {
 	static DCache dataCache;
 	static ICache instructionCache;
 
-	public static void initMemoryHandler(int memoryCycles, int org, ArrayList<String> instructions,
-			ArrayList<String> data, int cacheNumber, ArrayList<Integer> cycles, ArrayList<Integer> cacheSize,
-			ArrayList<Integer> lineSize, ArrayList<Integer> associativity, ArrayList<DCache.WritePolicy> dWritePolicy,
+	public static void initMemoryHandler(int memoryCycles, int org,
+			ArrayList<String> instructions, ArrayList<String> data,
+			int cacheNumber, ArrayList<Integer> cycles,
+			ArrayList<Integer> cacheSize, ArrayList<Integer> lineSize,
+			ArrayList<Integer> associativity,
+			ArrayList<DCache.WritePolicy> dWritePolicy,
 			ArrayList<ICache.WritePolicy> iWritePolicy) {
 		memory = new Memory(memoryCycles, org, instructions, data);
 		DCache.memory = memory;
 		DCache currentCache = dataCache;
 		for (int i = 0; i < cacheNumber; ++i) {
-			currentCache = new DCache(cycles.get(i), cacheSize.get(i), lineSize.get(i), associativity.get(i),
-					dWritePolicy.get(i));
+			currentCache = new DCache(cycles.get(i), cacheSize.get(i),
+					lineSize.get(i), associativity.get(i), dWritePolicy.get(i));
 			currentCache = currentCache.next;
 		}
 		ICache currentCache1 = instructionCache;
-		for (int i = 0; i < cacheNumber; ++i) {
-			currentCache1 = new ICache(cycles.get(i).intValue(), cacheSize.get(i).intValue(),
-					lineSize.get(i).intValue(), associativity.get(i).intValue(), iWritePolicy.get(i));
+		for (int i = 1; i < cacheNumber; ++i) {
+			currentCache1 = new ICache(cycles.get(i), cacheSize.get(i),
+					lineSize.get(i), associativity.get(i), iWritePolicy.get(i));
 			currentCache1 = currentCache1.next;
 		}
 	}
 
-	public static String read(int address) {
+	public static String readData(int address) {
 		DCache.time = 0;
-		return dataCache.read(address);
+		if (dataCache != null) {
+			return dataCache.read(address);
+		}
+		DCache.time = memory.cycles;
+		return memory.loadData(address);
 	}
 
-	public static void write(int address, String data) {
+	public static void writeData(int address, String data) {
 		DCache.time = 0;
 		dataCache.write(address, data);
+	}
+
+	public static Instruction readInstruction(int address) {
+		ICache.time = 0;
+		if (instructionCache != null) {
+			return instructionCache.read(address);
+		}
+		ICache.time = memory.cycles;
+		return memory.loadInstruction(address);
+	}
+
+	public static void writeInstruction(int address, String data) {
+		ICache.time = 0;
+		instructionCache.write(address, data);
 	}
 }
